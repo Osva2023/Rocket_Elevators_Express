c776d7d3461cb1215d4d6dfdeb5bc4dddf959f4a
Osvaldo Muguercia <elosvadel84@gmail.com>
2024-01-29 22:39:21 -0500

    add endpoints
    /hello
    /status
    /error
    /email-list

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = 3000
+const dotenv = require("dotenv");
+const express = require("express");
+const fs = require("fs");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+const environment = process.env.environment || "development";
+
 app.listen(port, () => {
-  console.log(` server listening on port ${port} `)
-})
+  console.log(`Server listening on port ${port} in ${environment} environment`);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/status", (req, res) => {
+  res.send(`Server is running on port ${port} in ${environment} environment`);
+});
+
+app.get("/email-list", async (req, res) => {
+  try {
+    const dataBuffer = await fs.promises.readFile("rocket-agents/agents.json");
+    const dataJSON = JSON.parse(dataBuffer);
+    
+    const emailList = dataJSON.map((agent) => agent.email).join(",");
+    res.send(emailList);
+  } catch (error) {
+    console.error("Error reading file", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+app.get("/error", (req, res) => {
+  res.status(500).send("Internal Server Error");
+});
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+});
