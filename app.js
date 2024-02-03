e1f6d55fbe86a13ecff70a799f58499ccfee0d3d
Osvaldo Muguercia <elosvadel84@gmail.com>
2024-02-02 21:10:05 -0500

    reviewed and changes made

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const fs = require("fs");
-const calculations = require("./calculations"); 
+const calculations = require("./calculations");
 
-dotenv.config();
+//dotenv.config({ path: "rocket_express.env"});
 
+console.log('process.env.PORT:', process.env.PORT); // Debug line
 
-const {standard_price, premium_price, exelium_price, standard_fee, premium_fee, exelium_fee} = require("./appData");
+const {
+  standard_price,
+  premium_price,
+  exelium_price,
+  standard_fee,
+  premium_fee,
+  exelium_fee,
+  
+} = require("./appData");
 const appData = require("./appData");
-
+const dataArray = appData.dataArray;
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT  || 3000;
+console.log('port:', port);                                             // Debug line
 const environment = process.env.environment || "development";
 
 app.listen(port, () => {
@@ -30,7 +40,6 @@ app.get("/status", (req, res) => {
 
 app.get("/email-list", async (req, res) => {
   try {
-    
     const emailList = appData.dataArray.map((agent) => agent.email).join(",");
     res.status(200).send(emailList);
   } catch (error) {
@@ -39,17 +48,15 @@ app.get("/email-list", async (req, res) => {
   }
 });
 
-app.get("/region-avg/:region", async (req, res) => {
-  const region = req.params.region;
-
+app.get("/region-avg", async (req, res) => {
+  const region = req.query.region;
+  console.log("Request received ", region); // Debugging purposes
   try {
-    const dataBuffer = await fs.promises.readFile("agents.json");
-    const dataJSON = JSON.parse(dataBuffer);
-
-    const regionAgents = dataJSON.filter(
+    
+    const regionAgents = dataArray.filter(
       (agent) => agent.region.trim().toLowerCase() === region
     );
-
+    console.log("Agents filtered ", regionAgents); // Debugging purposes
     if (regionAgents.length === 0) {
       res.status(404).send("Not agents were found in the Region provided.");
       return;
@@ -76,8 +83,8 @@ app.get("/calc-residential/:apartment/:floors/:tier", async (req, res) => {
   const tier = req.params.tier;
   const floors = req.params.floors;
   const apartment = req.params.apartment;
-  
-  console.log("parsed values ", {tier, floors, apartment });   // Debugging purposes
+
+  console.log("parsed values ", { tier, floors, apartment }); // Debugging purposes
   if (tier !== "standard" && tier !== "premium" && tier !== "exelium") {
     res.status(400).send("Invalid Tier");
     return;
@@ -88,7 +95,7 @@ app.get("/calc-residential/:apartment/:floors/:tier", async (req, res) => {
       .send("Invalid input: floors and apartment should be numbers");
     return;
   }
-  console.log("values checked ", {tier, floors, apartment });   // Debugging purposes
+  console.log("values checked ", { tier, floors, apartment }); // Debugging purposes
   const floorsNumber = Number(floors);
   const apartmentNumber = Number(apartment);
 
@@ -97,7 +104,6 @@ app.get("/calc-residential/:apartment/:floors/:tier", async (req, res) => {
       .status(400)
       .send("Invalid input: floors and apartment should be integers");
     return;
-    
   }
   if (floorsNumber <= 0 || apartmentNumber <= 0) {
     res
@@ -105,21 +111,37 @@ app.get("/calc-residential/:apartment/:floors/:tier", async (req, res) => {
       .send("Invalid input: floors and apartment should be greater than zero");
     return;
   }
-  console.log("new values checked ", {floorsNumber, apartmentNumber });   // Debugging purposes
+  console.log("new values checked ", { floorsNumber, apartmentNumber }); // Debugging purposes
 
-  const result = calculations.getCost(tier, floors, apartment, standard_price, premium_price, exelium_price, standard_fee, premium_fee, exelium_fee);
-
-    console.log("processing completed, sendind response", result);   // Debugging purposes
-    res.status(200).send({ cost: result });
-
+  const result = calculations.getCost(
+    tier,
+    floors,
+    apartment,
+    standard_price,
+    premium_price,
+    exelium_price,
+    standard_fee,
+    premium_fee,
+    exelium_fee
+  );
+  number_of_elevators = calculations.calculateEle(apartmentNumber, floorsNumber);
+  
+  res.status(200).send({ "Elevators Needed:": number_of_elevators, cost: result });
+  
 });
 
 app.post("/contact-us", (req, res) => {
   const { first_name, last_name, message } = req.body;
   if (first_name && last_name && message) {
-    res.status(200).send(req.body);
-    } else {
-    res.status(400).send("Invalid input: first_name, last_name and message are required");
+    const responseData = req.body;
+
+    res.status(200).json( {
+      log: "Message received", responseData,
+      });
+  } else {
+    res
+      .status(400)
+      .send("Invalid input: first_name, last_name and message are required");
     return;
   }
 });
